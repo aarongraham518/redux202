da3e506dc50d1e3feb93bf14a04964ed5eb53b0f
Aaron Graham <aarongraham518@gmail.com>
2020-09-07 19:49:19 -0400

    state is now captured in postform component and checked using react dev tools in chrome, moving on to new posts

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react'
 class Postform extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            title: '',
+            body: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        });
     }
 
     render() {
@@ -13,12 +24,20 @@ class Postform extends Component {
                 <form>
                     <div>
                         <label>Title: </label><br/>
-                        <input type="text"/>
+                        <input 
+                            type="text"
+                            name="title"
+                            onChange={this.onChange}
+                            value={this.state.title}/>
                     </div>
+                    <br/>
 
                     <div>
                         <label>Body: </label><br/>
-                        <textarea name="body"/>
+                        <textarea 
+                            name="body"
+                            onChange={this.onChange}
+                            value={this.state.body}/>
                     </div>
                     <br/>
                     <button type="submit">Submit</button>
