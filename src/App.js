c3f91b79e479f6d4c9514f64edc5f585477b2f3d
Aaron Graham <aarongraham518@gmail.com>
2020-09-07 18:39:44 -0400

    added Postform component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Posts from './components/Posts';
+import Postform from './components/Postform';
+
 
 function App() {
   return (
     <div className="App">
-      <Posts/>
+    <Postform/>
+    <hr/>
+    <Posts/>
     </div>
   );
 }
